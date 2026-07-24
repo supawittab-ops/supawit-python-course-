f994e988d31c6736608443a3db8898736c591192
Supawit <supawit.tab@ku.th>
2026-07-24 12:26:27 +0700

    loop error

--- assignments/week03/quiz-atm.py
+++ assignments/week03/quiz-atm.py
@@ -1,24 +1,3 @@
-# Complete this program to classify people by age
-age = int(input("Enter age: "))
-
-# Add your if-elif-else statements here
-# 0-12: Child
-# 13-19: Teenager  
-# 20-59: Adult
-# 60+: Senior
-
-# Your code here:
-
-if age <= 12:
-    print("Child")
-elif age <= 19:
-    print("Teenager")
-elif age <= 59:
-    print("Adult")
-else:
-    print("Senior")
-
-
 # Complete this ATM simulation
 balance = 1000
 pin = "1234"
